cdd47fb3a85edf4b787566e42a6bfd3bd8c8ab9c
Wilson Naut <Wilson.naut@gmail.com>
2020-05-19 19:26:12 -0400

    First commit

---
+++ MorseCode/MorseTranslator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MorseCode
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MorseTranslator
+    {
+        private readonly Dictionary<string, string> morseDictionary = new Dictionary<string, string>
+        {
+            ["E"] = ".",
+            ["SOS"] = "...|---|...",
+            ["SOS SOS"] = "...|---|... ...|---|...",
+            ["ADERLIS Y EDWIN"] = ".-|-..|.|.-.|.-..|.|... -.-- .|-..|.--|.|-.",
+            ["WILSON Y OMAR"] = ".--|.|.-..|...|---|-. -.-- ---|--|.-|.-.",
+            ["JACKSON Y ROBERT"] =  ".---|.-|-.-.|-.-|...|---|-. -.-- .-.|---|-...|.|.-.|-",
+            ["SCRUM DEVELOPER"] =  "...|-.-.|.-.|..-|-- -..|.|...-|.|.-..|---|.--.|.|.-.",
+            ["ALOES PMS"] =  ".-|.-..|---|.|... .--.|--|..."
+        };
+
+        public string Translate(string keyMessage)
+        {
+            if (keyMessage == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (string.IsNullOrEmpty(keyMessage) || string.IsNullOrWhiteSpace(keyMessage))
+            {
+                return "";
+            }
+
+            morseDictionary.TryGetValue(keyMessage, out string morseCode);
+
+            return morseCode;
+        }
+    }
+}
---
+++ MorseCodeTest/MorseTranslatorTest.cs
@@ -0,0 +1,52 @@
+﻿using MorseCode;
+using NUnit.Framework;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace MorseCodeTest
+{
+    /*
+     * PO Test Cases:
+     * [TestCase("", "")] DONE
+     * [TestCase("E", ".")] DONE
+        [TestCase("SOS", "...|---|...")]
+        [TestCase("SOS SOS", "...|---|... ...|---|...")]
+        c
+        [TestCase("WILSON Y OMAR", ".--|.|.-..|...|---|-. -.-- ---|--|.-|.-.")]
+        [TestCase("JACKSON Y ROBERT", ".---|.-|-.-.|-.-|...|---|-. -.-- .-.|---|-...|.|.-.|-")]
+        [TestCase("SCRUM DEVELOPER", "...|-.-.|.-.|..-|-- -..|.|...-|.|.-..|---|.--.|.|.-.")]
+        [TestCase("ALOES PMS", ".-|.-..|---|.|... .--.|--|...")]
+         [Test]
+
+     */
+    [TestFixture]
+    public class MorseTranslatorTest
+    {
+        [TestCase(" ", "")]
+        [TestCase("", "")]
+        [TestCase("ALOES PMS", ".-|.-..|---|.|... .--.|--|...")]
+        [TestCase("SCRUM DEVELOPER", "...|-.-.|.-.|..-|-- -..|.|...-|.|.-..|---|.--.|.|.-.")]
+        [TestCase("JACKSON Y ROBERT", ".---|.-|-.-.|-.-|...|---|-. -.-- .-.|---|-...|.|.-.|-")]
+        [TestCase("WILSON Y OMAR", ".--|.|.-..|...|---|-. -.-- ---|--|.-|.-.")]
+        [TestCase("ADERLIS Y EDWIN", ".-|-..|.|.-.|.-..|.|... -.-- .|-..|.--|.|-.")]
+        [TestCase("SOS SOS", "...|---|... ...|---|...")]
+        [TestCase("SOS", "...|---|...")]
+        [TestCase("E", ".")]
+        public void String_Message_Return_Morse_Code(string message, string morseCode)
+        {
+            var translator = new MorseTranslator();
+
+            string response = translator.Translate(message);
+
+            Assert.That(response, Is.EqualTo(morseCode));
+        }
+
+        [Test]
+        public void Null_Message_Return_ArgumentNullException()
+        {
+            var translator = new MorseTranslator();
+
+            Assert.Throws(Is.TypeOf<ArgumentNullException>(), () => translator.Translate(null));
+        }
+    }
+}
